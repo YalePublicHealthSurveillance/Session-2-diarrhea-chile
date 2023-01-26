5f40db94d858d10e45fbc72df601e5418be2b130
DanWeinberger <daniel.weinberger@yale.edu>
2023-01-26 03:53:46 +0000

    focus onage group 2 where there is more obvious epidemic

--- R/surv.ds.convert.R
+++ R/surv.ds.convert.R
@@ -1,10 +1,12 @@
 surv.ds.convert <- function(ds, datevar='date', casevar='cases'){
-  year.start <- min(year(ds[,datevar]))
-  week.start <- min(week(ds[,datevar])[year(ds[,datevar])==year.start])
+  datevec <- pull(ds, datevar)
+  casevec <- pull(ds, casevar)
+  year.start <- min(year(datevec))
+  week.start <- min(week(datevec)[year(datevec)==year.start])
   
   SurvObj <- create.disProg(
     week = 1:nrow(ds), #Index of observations
-    observed = ds[,casevar] ,  #cases
+    observed = casevec ,  #cases
     state=matrix(0, nrow=nrow(ds), ncol=1), #just 0s
     start = c(year.start, week.start)) #start date; 1st week of 1990
   return(SurvObj)
